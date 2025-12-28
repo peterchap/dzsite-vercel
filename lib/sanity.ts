5d78bb9bacbde3271aafc5bd0ab9f226a266b5b5
Peter Chaplin <peterchaplin@btinternet.com>
2025-12-28 08:27:51 +0000

    fixeed dataset normalization

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,11 +1,12 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
+import { apiVersion, dataset, projectId } from '@/sanity/env'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: '2024-01-01',
-  useCdn: true, // `false` if you want to ensure fresh data
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
 })
 
 const builder = imageUrlBuilder(client)
