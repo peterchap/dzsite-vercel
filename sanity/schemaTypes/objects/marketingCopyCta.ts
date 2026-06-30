483aa78c1fbd7a9f37fc7aebdddc4ddeb8b5fcf7
Peter Chaplin <42451267+peterchap@users.noreply.github.com>
2026-06-30 17:12:01 +0100

    Add reusable marketing copy schema

---
+++ sanity/schemaTypes/objects/marketingCopyCta.ts
@@ -0,0 +1,9 @@
+export default {
+    name: "marketingCopyCta",
+    title: "CTA",
+    type: "object",
+    fields: [
+        { name: "label", title: "Label", type: "string" },
+        { name: "href", title: "URL", type: "string" },
+    ],
+};
